0e93712183cb99d0c73fde92f735ad26b421c23d
Mohanad Elhag <moh.7afiz@gmail.com>
2017-10-31 11:26:48 +0300

    created item service

---
+++ src/app/models/Item.ts
@@ -0,0 +1,6 @@
+export interface Item {
+    // ? means optional
+    id?: string;
+    title?: string;
+    description?: string;
+}
---
+++ src/app/services/item.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Item } from '../models/Item';
+import {Observable} from 'rxjs/Observable';
+@Injectable()
+export class ItemService {
+  itemsCollection: AngularFirestoreCollection<Item>;
+  items: Observable<Item[]>;
+
+  constructor(public afs: AngularFirestore) { 
+    // valueChange() return collection data from Firebase
+    this.items = this.afs.collection('items').valueChanges();
+  }
+
+  getItems() {
+    return this.items;
+  }
+}
+
+
+
